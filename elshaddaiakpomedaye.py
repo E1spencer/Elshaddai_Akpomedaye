7641343c982c23d85bc27885228e2281cd682fed
E1spencer <elshaddaiakpomedaye@gmail.com>
2022-06-12 23:19:04 +0100

    first push to github - django installed python environment

---
+++ elshaddaiakpomedaye.py
@@ -0,0 +1,11 @@
+# ADDITION
+print("ADDITION")
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The addition of those two numbers =", x + y, "\n")
+
+# SUBTRACTION
+print("SUBTRACTION")
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+print("The subtraction of those two numbers =", x - y, "\n")
